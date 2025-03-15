5fbeb1552e1e49cb82cde6e4a4bc5b7ebd9a4483
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-03-14 21:04:57 -0400

    Add new elements: A, Form, Li, Nav, and Ul to the elements module

--- src/viol/elements/__init__.py
+++ src/viol/elements/__init__.py
@@ -1,7 +1,12 @@
+from .a import A
 from .button import Button
 from .div import Div
+from .form import Form
 from .header import H1, H2, H3, H4, H5, H6, Header
+from .li import Li
+from .nav import Nav
 from .span import Span
+from .ul import Ul
 
 __all__ = [
     "H1",
@@ -10,8 +15,13 @@ __all__ = [
     "H4",
     "H5",
     "H6",
+    "A",
     "Button",
     "Div",
+    "Form",
     "Header",
+    "Li",
+    "Nav",
     "Span",
+    "Ul",
 ]
