3fb720ef405ec8500eb9ff6ff076f52d1e69e55a
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-03-22 23:37:02 -0400

    Add htmx confirmation modal functionality for user interactions

---
+++ src/viol/static/js/htmx-bootstrap.js
@@ -0,0 +1,33 @@
+let violConfirmModalInstance = new bootstrap.Modal("#violConfirmModal");
+let violConfirmModalInstanceElement = document.getElementById(
+    "violConfirmModal",
+);
+let violConfirmModalConfirmButton = document.getElementById(
+    "violConfirmModalConfirmButton",
+);
+
+let violConfirmModalMessage = document.getElementById(
+    "violConfirmModalMessage",
+);
+
+// once hidden.bs.modal remove the event listener
+violConfirmModalInstanceElement.addEventListener(
+    "hidden.bs.modal",
+    () => {
+        violConfirmModalConfirmButton.onclick = null;
+    },
+);
+
+document.body.addEventListener("htmx:confirm", function (evt) {
+    if (~evt.target.matches("[confirm-with-native-alert='true']")) {
+        evt.preventDefault();
+        // update message in the model (#violConfirmModalMessage)
+        violConfirmModalMessage.innerText = evt.detail.question;
+        violConfirmModalInstance.show();
+        // add new event listener to the confirm button
+        violConfirmModalConfirmButton.onclick = () => {
+            violConfirmModalInstance.hide();
+            evt.detail.issueRequest(true);
+        };
+    }
+});
