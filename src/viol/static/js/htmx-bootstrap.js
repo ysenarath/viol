f6b035729e6e81fc05309850fae06225244d34b5
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-03-23 01:14:22 -0400

    Enhance htmx-bootstrap.js to check for hx-confirm attribute before using native alert

--- src/viol/static/js/htmx-bootstrap.js
+++ src/viol/static/js/htmx-bootstrap.js
@@ -19,15 +19,18 @@ violConfirmModalInstanceElement.addEventListener(
 );
 
 document.body.addEventListener("htmx:confirm", function (evt) {
-    if (~evt.target.matches("[confirm-with-native-alert='true']")) {
-        evt.preventDefault();
-        // update message in the model (#violConfirmModalMessage)
-        violConfirmModalMessage.innerText = evt.detail.question;
-        violConfirmModalInstance.show();
-        // add new event listener to the confirm button
-        violConfirmModalConfirmButton.onclick = () => {
-            violConfirmModalInstance.hide();
-            evt.detail.issueRequest(true);
-        };
+    // check hx-confirm attribute to see if it should use the native alert
+    if ("hx-confirm" in evt.target.attributes) {
+        if (~evt.target.matches("[confirm-with-native-alert='true']")) {
+            evt.preventDefault();
+            // update message in the model (#violConfirmModalMessage)
+            violConfirmModalMessage.innerText = evt.detail.question;
+            violConfirmModalInstance.show();
+            // add new event listener to the confirm button
+            violConfirmModalConfirmButton.onclick = () => {
+                violConfirmModalInstance.hide();
+                evt.detail.issueRequest(true);
+            };
+        }
     }
 });
