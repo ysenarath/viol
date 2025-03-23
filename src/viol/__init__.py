bb4c0a10e99b8c3a506bbf7ab7b3114467738736
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-03-22 23:37:25 -0400

    Refactor imports in __init__.py to include Layout and init_app, removing Server

--- src/viol/__init__.py
+++ src/viol/__init__.py
@@ -1,9 +1,10 @@
 from viol.core import render
-from viol.server import Server
+from viol.layout import Layout, init_app
 
 __version__ = "0.0.1"
 
 __all__ = [
-    "Server",
+    "Layout",
+    "init_app",
     "render",
 ]
